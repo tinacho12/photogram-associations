29fc61b71928c9a20f4d9860f0ac1bc199c6af53
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-27 18:58:12 +0000

    Updated comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -21,6 +21,11 @@ class Comment < ApplicationRecord
 
   # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
 
+  belongs_to(:commenter, class_name: "User", foreign_key: "author_id")
+
+  belongs_to(:photo, class_name: "Photo", foreign_key: "photo_id")
+
+=begin
   def commenter
     my_author_id = self.author_id
 
@@ -30,4 +35,6 @@ class Comment < ApplicationRecord
 
     return the_user
   end
+=end
+
 end
